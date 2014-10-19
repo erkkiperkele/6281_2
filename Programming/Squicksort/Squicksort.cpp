6ffacba75ce56466435e224b06287f204653a6dc
erkkiperkele <aymeric.grail@gmail.com>
2014-10-18 20:11:21 -0400

    Serial sort done

--- Programming/Squicksort/Squicksort.cpp
+++ Programming/Squicksort/Squicksort.cpp
@@ -1,21 +1,90 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <cstdlib>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+void quicksort(vector<int>& values);
+vector<int> LoadFromFile();
+int compare(const void * a, const void * b);
+void Print(vector<int>& values);
+void PrintChrono(double &duration, int qty);
+
+//TODO:
+//CHRONO
+//Output chrono
+
+int main()
+{
+	high_resolution_clock::time_point startTime = high_resolution_clock::now();
+
+	vector<int> values = LoadFromFile();
+
+	quicksort(values);
+
+	high_resolution_clock::time_point endTime = high_resolution_clock::now();
+	double time_span = duration_cast<duration<double> >(endTime - startTime).count();
+
+	cout << values.size() << " numbers (quick)sorted" << endl;
+	cout << "Duration: " << time_span << " seconds" << endl;
+	Print(values);
+	PrintChrono(time_span, values.size());
+
+	return 0;
+}
+
+void quicksort(vector<int>& values)
+{
+	qsort(&values[0], values.size(), sizeof(int), compare);
+}
+
+vector<int> LoadFromFile()
+{
+	ifstream input("./input.txt", ios::in);
+
+	int number;
+	vector<int> numbers;
+	
+	if (input.is_open())
+	{
+		while(input >> number)
+		{
+			numbers.push_back(number);
+			input.get();
+		}
+		input.close();
+	}
+	return numbers;
+}
 
 int compare(const void * a, const void * b)
 {
 	return (*(int*)a - *(int*)b);
 }
 
-int main()
+void Print(vector<int>& values)
 {
-	quicksort(values, valuesSize);
-	return 0;
+	std::ofstream myfile;
+	myfile.open("./output.txt");
+
+	int i = 0;
+	while (i < values.size())
+	{
+		myfile << values[i] << endl;
+		++i;
+	}
+
+	myfile.close();
 }
 
-void quicksort(int values[], valuesSize)
+void PrintChrono(double &duration, int qty)
 {
-	int n;
-	qsort(values, valuesSize, sizeof(int), compare);
-	for (n=0; n<valuesSize; n++)
-		printf("%d", values[n]);
+	std::ofstream myfile;
+	myfile.open("./SquicksortChrono.csv", ios::app);
+	myfile << "1," << qty << "," << duration << "\n";
+	myfile.close();
 }
