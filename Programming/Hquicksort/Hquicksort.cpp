345bf8a0f6d70121eeffbb7bded281e97a88c5f9
erkkiperkele <aymeric.grail@gmail.com>
2014-10-26 12:24:36 -0400

    Hquicksort in progress: compiling

--- Programming/Hquicksort/Hquicksort.cpp
+++ Programming/Hquicksort/Hquicksort.cpp
@@ -17,7 +17,7 @@ void quicksort(vector<int>& values);
 int compare(const void * a, const void * b);
 
 //Hquicksort
-void GetSubArraysPos(int pos[], int valuesSize, int qty);
+void GetSubArraysPos(int* pos[], int valuesSize, int qty);
 void HyperQSort();		//Temp signature
 
 //outputs
@@ -27,11 +27,11 @@ void PrintChrono(int &nodes, size_t qty, double &duration);
 int d;
 int p;
 int idle;
+vector<int> values;
 
 int main(int argc, char* argv[])
 {
-	
-	vector<int> values = LoadFromFile();
+
 		
 	int mpiRank; 
 	int mpiSize;
@@ -40,27 +40,48 @@ int main(int argc, char* argv[])
 	MPI_Comm_rank(MPI_COMM_WORLD, &mpiRank);
 	MPI_Comm_size(MPI_COMM_WORLD, &mpiSize);
 	
-	d = log2(mpiSize - 1);		
-	p = pow(2, d);				//Number of sorting processes
-	idle = mpiSize - 1 - p;		//number of idle processes 
-
-	//positions is an array of array[2] containing beginning and end of sub array.
-	int* positions[p];
-	GetSubArraysPos(positions, values.size(), p);
+	//REFACTOR: Organize code in different methods --------------------------
 	
-	double startTime = MPI_Wtime();
 	
 	if (mpiRank == 0)
 	{
-		//TODO
-		int rank = 0;
-		while (rank < p)		//PERF: Use Boradcast?
+		//STEP0: Start Chrono
+		double startTime = MPI_Wtime();
+		
+		d = log2(mpiSize);		//TODO: is P0 part of the hypercube?
+		p = pow(2, d);			//Number of sorting processes
+		idle = mpiSize - p;		//number of idle processes 
+
+		//positions is an array of array[2] containing beginning and end of sub array.
+		int* positions[p];
+		GetSubArraysPos(positions, values.size(), p);
+	
+		cout << positions[0][0] << positions[0][1] << endl;
+	
+	
+		//STEP1: Read input
+		values = LoadFromFile();
+		int pivot0 = values[0];		//Arbitrary pivot
+		
+		//STEP2: Distribute data
+		int currentP = 0;
+		while (currentP < p)		//PERF: Use Boradcast!!
 		{
-			int size = positions[rank-1][1] - positions[rank-1][0];
-			MPI_Send(&values[rank-1][0], size, MPI_INT, id, 0, MPI_COMM_WORLD);
+			int size = positions[currentP][1] - positions[currentP][0];
+			// MPI_Send(&values[currentP][0], size, MPI_INT, id, 0, MPI_COMM_WORLD);
+			++currentP;
 		}
 		
-		HyperQSort(&values[0], values.size());
+		// HyperQSort(&values[0], values.size());
+		
+		//STEPX: Stop chrono and print results
+		double endTime = MPI_Wtime();
+		double duration = endTime - startTime;
+
+		cout << values.size() << " numbers (quick)sorted" << endl;
+		cout << "Duration: " << duration << " seconds" << endl;
+		Print(values);
+		PrintChrono(mpiSize, values.size(), duration);
 	}
 
 	if (mpiRank != 0)
@@ -70,14 +91,7 @@ int main(int argc, char* argv[])
 		
 	//void HyperQSort();	
 
-	double endTime = MPI_Wtime();
-	double duration = endTime - startTime;
-
-	cout << values.size() << " numbers (quick)sorted" << endl;
-	cout << "Duration: " << duration << " seconds" << endl;
-	Print(values);
-	PrintChrono(mpiSize, values.size(), duration);
-
+	MPI_Finalize();
 	return 0;
 }
 
@@ -91,68 +105,71 @@ void HyperQSort(int array[], int arraySize)
 	
 	
 	int i = 0;
-	while (i < d)
-	{
-		if (mpiRank == 0)
-		{
-			//Arbitrary pivot value taken in the middle in case values are already ordered.
-			int pivot = array[arraySize / 2];
-			
-			//broadcast pivot
-		}
-		
-		vector<int> arrayL;
-		vector<int> arrayU;
-		
-		int j = 0;
-		while (j < arraySize)
-		{
-			if(array[j] <= pivot)
-			{
-				arrayL.push_back(array[j]);
-			}
-			else
-			{
-				arrayU.push_back(array[j]);
-			}
-			++j;
-		}
-		
-		int destId = mpiRank ^ pow(2, i);
-		
-		if (rank & (1<<i))	//checks if nth bit is set
-		{
-			//send lower
-			//receive from upper
-			//array = arrayU U receivedArray
-		}
-		else 
-		{
-			//send upper
-			//etc...
-		}
-		++i;
-	}
+	// while (i < d)
+	// {
+	// 	if (mpiRank == 0)
+	// 	{
+	// 		//Arbitrary pivot value taken in the middle in case values are already ordered.
+	// 		int pivot = array[arraySize / 2];
+	//
+	// 		//broadcast pivot
+	// 	}
+	//
+	// 	vector<int> arrayL;
+	// 	vector<int> arrayU;
+	//
+	// 	int j = 0;
+	// 	while (j < arraySize)
+	// 	{
+	// 		if(array[j] <= pivot)
+	// 		{
+	// 			arrayL.push_back(array[j]);
+	// 		}
+	// 		else
+	// 		{
+	// 			arrayU.push_back(array[j]);
+	// 		}
+	// 		++j;
+	// 	}
+	//
+	// 	int destId = mpiRank ^ pow(2, i);
+	//
+	// 	if (rank & (1<<i))	//checks if nth bit is set
+	// 	{
+	// 		//send lower
+	// 		//receive from upper
+	// 		//array = arrayU U receivedArray
+	// 	}
+	// 	else
+	// 	{
+	// 		//send upper
+	// 		//etc...
+	// 	}
+	// 	++i;
+	// }
 	
 }
 
-void GetSubArraysPos(int* pos[], int valuesSize, int qty)
+void GetSubArraysPos(int* pos[], int valuesSize, int processes)
 {
 	int i = 0;
-	int arraySize = valuesSize / (qty);
+	int arraySize = valuesSize / processes;
 	
-	while (i < qty)
+	while (i < processes)
 		{
 			//each value of pos is the position of the beginning and end of each subarray
 			int arrayLimits[2];
 			arrayLimits[0] = i * arraySize;			//Starting position
-			arrayLimits[1] = i == (qty - 1)			//Ending postion. Last array contains any remaining values.
+			arrayLimits[1] = i == (processes - 1)	//Ending postion. Last p contains remaining values.
 				? valuesSize - 1
-				: arrayLimits[0] + arraySize
-			positions[i] = arrayLimits;
+				: arrayLimits[0] + arraySize;
+			pos[i] = arrayLimits;
 			++i;
 		}
 }
+
+
+//Helpers, output and serial operations --------------------------
 
 void quicksort(vector<int>& values)
 {
